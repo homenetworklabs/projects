d356aea7158f0721f58785da17325afd571ad338
homenetworklabs <41763724+homenetworklabs@users.noreply.github.com>
2018-10-12 13:09:01 -0500

    Cosmetic changes...removed all default banner and wordings.

--- DNS-GUI-REQ/DNS-GUI-REQ/Controllers/HomeController.cs
+++ DNS-GUI-REQ/DNS-GUI-REQ/Controllers/HomeController.cs
@@ -15,14 +15,13 @@ namespace DNS_GUI_REQ.Controllers
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
-
+		
 			return View();
 		}
 
 		public ActionResult Contact()
 		{
-			ViewBag.Message = "Your contact page.";
+			
 
 			return View();
 		}

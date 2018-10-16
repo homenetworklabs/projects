03b59477611814c5ac3fd8aebdb35846bf7b09cb
homenetworklabs <41763724+homenetworklabs@users.noreply.github.com>
2018-10-16 10:59:16 -0500

    homecontroller cleanup

--- DNS-GUI/DNS-GUI/Controllers/HomeController.cs
+++ DNS-GUI/DNS-GUI/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace DNS_GUI.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+         
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+       
 
             return View();
         }
